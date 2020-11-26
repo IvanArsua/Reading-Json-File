97581dd0c8c160d0de50f194896ae3b2370fcd2c
Ivan Joshua Punzalan Arsua <iparsua@csmbataan.ph.education>
2020-11-26 15:10:03 -0800

    Finished Project

---
+++ TVLICT201.JSONReader/TVLICT201.JSONReader.Windows/Book/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVLICT201.JSONReader.Windows.Book
+{
+    public class Book
+    {
+        public string Id { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public string publisher { get; set; }
+        public string date { get; set; }
+        
+    }
+}
---
+++ TVLICT201.JSONReader/TVLICT201.JSONReader.Windows/Book/BookWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TVLICT201.JSONReader.Windows.Book
+{
+    /// <summary>
+    /// Interaction logic for BookWindow.xaml
+    /// </summary>
+    public partial class BookWindow : Window
+    {
+        public BookWindow()
+        {
+            InitializeComponent();
+            Book book = new Book();
+
+            using (StreamReader r = new StreamReader("data\\book.json"))
+            {
+                string json = r.ReadToEnd();
+                book = JsonConvert.DeserializeObject<Book>(json);
+            }
+           
+            lblBookId.Content = "Id :" + book.Id;
+            lblBookTitle.Content = "Title :" + book.title;
+            lblBookAuthor.Content = "Author :" + book.author;
+            lblBookPublisher.Content = "Publisher :" + book.publisher;
+            lblBookDate.Content = "Date :" + book.date;
+        }
+    }
+}
